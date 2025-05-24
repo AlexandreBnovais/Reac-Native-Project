ce8463870c5d6d98b3b88985c32c68178624dff4
AlexandreBnovais <157971881+AlexandreBnovais@users.noreply.github.com>
2025-05-23 21:01:14 -0300

    updata

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen } from '../screens/HomeScreen/homeScreen';
-import { LoginScreen } from '../screens/LoginScreen/loginScreen';
-import { singUpScreen } from '../screens/SingUpScreen/singUpScreen';
-import { TimeScreen } from '../screens/TimeLineScreen/timeScreen';
+import { HomeScreen } from '../../screens/HomeScreen/homeScreen';
+import { LoginScreen } from '../../screens/LoginScreen/loginScreen';
+import { singUpScreen } from '../../screens/SingUpScreen/singUpScreen';
+import { TimeScreen } from '../../screens/TimeLineScreen/timeScreen';
 
 const Stack = createNativeStackNavigator();
 
